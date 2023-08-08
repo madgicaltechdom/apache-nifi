732bbe0f52639a229c81b42e7bc2f8165114e1d3
Shubhammadgical <shubham.chaurasia@madgicaltechdom.com>
2023-08-08 17:47:10 +0530

    format the code and add the code for SG

--- main.tf
+++ main.tf
@@ -1,21 +1,52 @@
 provider "aws" {
-  region  = var.aws-region
+  region     = var.aws_region
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
-resource "aws_spot_instance_request" "instance" {
-  ami                         = "${var.instance-ami}"
-  instance_type               = "${var.instance-type}"
-  key_name                    = "${var.instance-key-name != "" ? var.instance-key-name : ""}"
-  subnet_id                   = "${var.subnet_id}"
- spot_price    = "${var.spot_price }"
-spot_type  = "${var.spot_type}"
-#vpc_security_group_ids      = ["${aws_security_group.sg.id}"]
-#  vpc_security_group_ids = var.vpc_security_group_ids
-  tags = {
-    Name = "${var.instance-tag-name}"
+resource "aws_security_group" "webserver-security-group" {
+  name        = "${var.application_name}-sg"
+  description = "create the Security Group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_spot_instance_request" "instance" {
+  ami                    = var.instance_ami
+  instance_type          = var.instance_type
+  key_name               = var.instance_key_name
+  subnet_id              = var.subnet_id
+  spot_price             = var.spot_price
+  vpc_security_group_ids = [aws_security_group.webserver-security-group.id]
+  spot_type              = var.spot_type
+  ebs_block_device {
+    device_name = "/dev/xvda"
+    volume_type = "gp3"
+    volume_size = 16 # Replace with the desired volume size in GB
+  }
+  tags = {
+    Name = "${var.instance_tag_name}"
+  }
+  depends_on = [aws_security_group.webserver-security-group]
 }
 
 
--- variable.tf
+++ variable.tf
@@ -1,47 +1,53 @@
-variable "aws-region" {
+variable "aws_region" {
   description = "The AWS region"
   default     = "ap-south-1"
 }
+
 variable "access_key" {
   description = "value"
   default     = ""
 }
+
 variable "secret_key" {
   description = "value"
   default     = ""
 }
-variable "instance-type" {
+
+variable "instance_type" {
     description= "value"
     default= "t2.medium"
 }
+
 variable "subnet_id" {
     description= "value"
-    default="subnet-0f87a45fe905355f1"
+    default="subnet-015e1c11f94538c9b"
 }
-variable "instance-ami" {
+
+variable "instance_ami" {
  description=  "value"
  default= "ami-08e5424edfe926b43"
 }
-variable "instance-key-name"{
+
+variable "application_name" {
+ description=  "machine name"
+ default= "Nowigence"
+}
+
+variable "instance_key_name"{
  description= "name of key"
  default = "nowigence-key"
 }
+
 variable "spot_price" {
   default = "0.03"
 }
+
 variable "spot_type" {
   description = "value"
   default     = "persistent"
 }
-variable "snapshot_without_reboot" {
-  default = "true"
-}
-variable "instance-tag-name"{
+
+variable "instance_tag_name"{
     description= "name of machine"
     default= "nowigence"
 }
- variable "vpc_security_group_ids"{
-     type = list(string)
-    description= "name of vpc"
-    default = ["vpc-017092b3559f639ed"]
- }
